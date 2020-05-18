e9b177b0314ad60527207497ac53fe243dd4b274
Damilare <damilare182@gmail.com>
2020-05-18 03:11:33 +0100

    added keycloak and authguards

--- angularkeycloak/src/app/app.module.ts
+++ angularkeycloak/src/app/app.module.ts
@@ -1,20 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+import { AppAuthGuard } from './app.authguard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
-
+import { FirstpageComponent } from './firstpage/firstpage.component';
+const keycloakService = new KeycloakService();
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    FirstpageComponent
   ],
   imports: [
+    KeycloakAngularModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AppAuthGuard, {
+    provide: KeycloakService,
+    useValue: keycloakService
+ }],
+ entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule { 
+  ngDoBootstrap(app) {
+    keycloakService
+      .init({
+        config: {
+          url: 'http://localhost:8080/auth',
+          realm: 'keycloak-angular',
+          clientId: 'angularproject',
+        },
+        initOptions: {
+          onLoad: 'login-required',
+          checkLoginIframe: false,
+        },
+        enableBearerInterceptor: true,
+        bearerExcludedUrls: [],
+      })
+      .then(() => {
+        app.bootstrap(AppComponent);
+      })
+      .catch((error) =>
+        console.error('[ngDoBootstrap] init Keycloak failed', error)
+      );
+  }
+}
--- angularkeycloak/src/environments/environment.ts
+++ angularkeycloak/src/environments/environment.ts
@@ -1,9 +1,17 @@
+import { KeycloakConfig } from 'keycloak-angular';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+let keycloakConfig: KeycloakConfig = {
+  url: 'http://localhost:8080/auth',
+  realm: 'keycloak-angular',
+  clientId: 'angularproject',
+};
 
 export const environment = {
-  production: false
+  production: false,
+  keycloak: keycloakConfig,
 };
 
 /*
