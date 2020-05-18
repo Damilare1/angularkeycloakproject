3706467eb7c968196cd2ad1e7411db3f61770433
Damilare <damilare182@gmail.com>
2020-05-18 02:10:19 +0100

    added routes

--- angularkeycloak/src/app/app-routing.module.ts
+++ angularkeycloak/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { XyzComponent } from './xyz/xyz.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'xyz',
+    component: XyzComponent ,
+    data: { roles: ['user'] },
+  },
+  {
+    path: '',
+    component: AppComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
